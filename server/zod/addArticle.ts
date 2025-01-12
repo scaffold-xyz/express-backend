1d23caa3f06bab5a81e9b4239de5eb09b9bf9077
xhakti <117899415+xhakti@users.noreply.github.com>
2025-01-12 15:00:10 +0530

    no different zod files are required

--- server/zod/addArticle.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const addArticleSchema = z.object({
-  title: z.string(),
-  content: z.string(),
-});
-
-export type InferAddArticleSchema = z.infer<typeof addArticleSchema>;
